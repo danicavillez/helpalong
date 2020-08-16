f4a9a01dfc1fdfbe4a42eb4b31443bd326d20c32
danicavillez <dvillez4@cs.washington.edu>
2020-08-16 00:10:09 -0700

    TRIED to fix dropdown

--- my-app/src/components/MentalHealth.js
+++ my-app/src/components/MentalHealth.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Dropdown from './layout/Dropdown'
 
 export default function MentalHealth() {
     return (
         <div>
             <h1> Mental Health Resources</h1>
-            <h2> What County are you located in? </h2>
-                <Dropdown />
+            <Dropdown />
             <h2> Do you need someone to talk to? </h2>
         </div>
     )
--- my-app/src/components/layout/Dropdown.js
+++ my-app/src/components/layout/Dropdown.js
@@ -1,50 +1,66 @@
-import React from 'react'
+import React from 'react';
 
-export default function Dropdown() {
-    return (
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                County Name</button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Adams</a>
-                <a class="dropdown-item" href="#">Asotin</a>
-                <a class="dropdown-item" href="#">Benton</a>
-                <a class="dropdown-item" href="#">Chelan</a>
-                <a class="dropdown-item" href="#">Clallam</a>
-                <a class="dropdown-item" href="#">Clark</a>
-                <a class="dropdown-item" href="#">Columbia</a>
-                <a class="dropdown-item" href="#">Cowlitz</a>
-                <a class="dropdown-item" href="#">Douglas</a>
-                <a class="dropdown-item" href="#">Ferry</a>
-                <a class="dropdown-item" href="#">Franklin</a>
-                <a class="dropdown-item" href="#">Garfield</a>
-                <a class="dropdown-item" href="#">Grant</a>
-                <a class="dropdown-item" href="#">Grays Harbor</a>
-                <a class="dropdown-item" href="#">Island</a>
-                <a class="dropdown-item" href="#">Jefferson</a>
-                <a class="dropdown-item" href="#">King</a>
-                <a class="dropdown-item" href="#">Kitsap</a>
-                <a class="dropdown-item" href="#">Kittitas</a>
-                <a class="dropdown-item" href="#">Klickitat</a>
-                <a class="dropdown-item" href="#">Lewis</a>
-                <a class="dropdown-item" href="#">Lincoln</a>
-                <a class="dropdown-item" href="#">Mason</a>
-                <a class="dropdown-item" href="#">Okanogan</a>
-                <a class="dropdown-item" href="#">Pacific</a>
-                <a class="dropdown-item" href="#">Pend Oreille</a>
-                <a class="dropdown-item" href="#">Pierce</a>
-                <a class="dropdown-item" href="#">Skagit</a>
-                <a class="dropdown-item" href="#">Skamania</a>
-                <a class="dropdown-item" href="#">Snohomish</a>
-                <a class="dropdown-item" href="#">Spokane</a>
-                <a class="dropdown-item" href="#">Stevens</a>
-                <a class="dropdown-item" href="#">Thurston</a>
-                <a class="dropdown-item" href="#">Wahkiakum</a>
-                <a class="dropdown-item" href="#">Walla Walla</a>
-                <a class="dropdown-item" href="#">Whatcom</a>
-                <a class="dropdown-item" href="#">Whitman</a>
-                <a class="dropdown-item" href="#">Yakima</a>
-            </div>
-        </div>
-    )
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'King'};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {
+        return(
+            <h2> What County are you located in? :
+            <select value={this.state.value} onChange={this.handleChange} classNameName="dropdown" id="county">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        County Name</button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <option className="dropdown-item" value="Adams"> Adams</option>
+                        <option className="dropdown-item" value="Asotin">Asotin</option>
+                        <option className="dropdown-item" value="Benton">Benton</option>
+                        <option className="dropdown-item" value="Chelan">Chelan</option>
+                        <option className="dropdown-item" value="Clallam">Clallam</option>
+                        <option className="dropdown-item" value="Clark">Clark</option>
+                        <option className="dropdown-item" value="Columbia">Columbia</option>
+                        <option className="dropdown-item" value="Cowlitz">Cowlitz</option>
+                        <option className="dropdown-item" value="Douglas">Douglas</option>
+                        <option className="dropdown-item" value="Ferry">Ferry</option>
+                        <option className="dropdown-item" value="Franklin">Franklin</option>
+                        <option className="dropdown-item" value="Garfield">Garfield</option>
+                        <option className="dropdown-item" value="Grant">Grant</option>
+                        <option className="dropdown-item" value="Grays Harbor">Grays Harbor</option>
+                        <option className="dropdown-item" value="Island">Island</option>
+                        <option className="dropdown-item" value="Jefferson">Jefferson</option>
+                        <option className="dropdown-item" value="King">King</option>
+                        <option className="dropdown-item" value="Kitsap">Kitsap</option>
+                        <option className="dropdown-item" value="Kittitas">Kittitas</option>
+                        <option className="dropdown-item" value="Klickitat">Klickitat</option>
+                        <option className="dropdown-item" value="Lewis">Lewis</option>
+                        <option className="dropdown-item" value="Lincoln">Lincoln</option>
+                        <option className="dropdown-item" value="Mason">Mason</option>
+                        <option className="dropdown-item" value="Okanogan">Okanogan</option>
+                        <option className="dropdown-item" value="Pacific">Pacific</option>
+                        <option className="dropdown-item" value="Pend Oreille">Pend Oreille</option>
+                        <option className="dropdown-item" value="Pierce">Pierce</option>
+                        <option className="dropdown-item" value="Skagit">Skagit</option>
+                        <option className="dropdown-item" value="Skamania">Skamania</option>
+                        <option className="dropdown-item" value="Snohomish">Snohomish</option>
+                        <option className="dropdown-item" value="Spokane">Spokane</option>
+                        <option className="dropdown-item" value="Stevens">Stevens</option>
+                        <option className="dropdown-item" value="Thurtson">Thurston</option>
+                        <option className="dropdown-item" value="Wahkiakum">Wahkiakum</option>
+                        <option className="dropdown-item" value="Walla Walla">Walla Walla</option>
+                        <option className="dropdown-item" value="Whatcom">Whatcom</option>
+                        <option className="dropdown-item" value="Whitman">Whitman</option>
+                        <option className="dropdown-item" value="Yakima">Yakima</option>
+                    </div>
+            </select>
+            </h2>
+        );
+    }
 }
+
+export default Dropdown;
